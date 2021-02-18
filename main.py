82f5f910ddc4034e08f067ea1f8adc7cd87b79e1
Joe Wlos <joewlos17@gmail.com>
2021-02-17 20:20:01 -0500

    App name

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 IMPORT FOR GUNICORN
 '''
 from api import app
-application = app.app
+app = app.app
 
 '''
 RUN IN TERMINAL
 '''
 # Run Flask on debug when local
 if __name__ == '__main__':
-    application.run(port=5000, debug=True)
+    app.run(port=5000, debug=True)
